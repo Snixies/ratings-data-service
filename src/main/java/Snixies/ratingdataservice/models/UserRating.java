c44eee929ecc9947b27f90ad49ad11d8a62cb830
Snixies <79220714+Snixies@users.noreply.github.com>
2021-05-19 19:40:26 +0200

    added objectwrapper for List<>

---
+++ src/main/java/Snixies/ratingdataservice/models/UserRating.java
@@ -0,0 +1,24 @@
+package Snixies.ratingdataservice.models;
+
+import java.util.List;
+
+public class UserRating {
+
+    private List<Rating> userRatings;
+
+    public UserRating(List<Rating> userRatings) {
+        this.userRatings = userRatings;
+    }
+
+    public UserRating() {
+
+    }
+
+    public List<Rating> getUserRatings() {
+        return userRatings;
+    }
+
+    public void setUserRatings(List<Rating> userRatings) {
+        this.userRatings = userRatings;
+    }
+}
--- src/main/java/Snixies/ratingdataservice/resources/RatingsResource.java
+++ src/main/java/Snixies/ratingdataservice/resources/RatingsResource.java
@@ -1,9 +1,13 @@
 package Snixies.ratingdataservice.resources;
 
 import Snixies.ratingdataservice.models.Rating;
+import Snixies.ratingdataservice.models.UserRating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/ratingsdata")
@@ -12,5 +16,16 @@ public class RatingsResource {
     @RequestMapping("/{beerId}")
     public Rating getRating (@PathVariable("beerId") int beerId){
         return new Rating(beerId, 4);
+    }
+
+    @RequestMapping("/users/{userId}")
+    public UserRating getUserRating (@PathVariable("userId") int userId){
+        List<Rating> ratings = Arrays.asList(
+                new Rating(1,4),
+                new Rating(2,5)
+        );
+        UserRating userRating = new UserRating();
+        userRating.setUserRatings(ratings);
+        return userRating;
     }
 }
